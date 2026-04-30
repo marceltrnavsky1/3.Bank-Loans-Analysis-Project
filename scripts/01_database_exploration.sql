ac14bdf8393f9bb796003995771be94a0ae09974
marceltrnavsky1 <marcel.trnavsky1@gmail.com>
2026-04-30 23:03:54 +0200

    Update 01_database_exploration.sql

--- scripts/01_database_exploration.sql
+++ scripts/01_database_exploration.sql
@@ -1,7 +1,17 @@
+-- Retrieve a list of all tables in the database
 SELECT 
-    COLUMN_NAME,
-    DATA_TYPE,
-    IS_NULLABLE
+    TABLE_CATALOG, 
+    TABLE_SCHEMA, 
+    TABLE_NAME, 
+    TABLE_TYPE
+FROM INFORMATION_SCHEMA.TABLES;
+
+-- Retrieve all columns for a table
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    CHARACTER_MAXIMUM_LENGTH
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'dbo'
   AND TABLE_NAME = 'financial_loan';
