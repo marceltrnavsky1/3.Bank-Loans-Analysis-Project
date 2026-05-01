b9d998ae65db39ef59a6b066d597fd0a97f74fe5
marceltrnavsky1 <marcel.trnavsky1@gmail.com>
2026-05-01 16:00:20 +0200

    Update 00_data_quality_check.sql

--- scripts/00_data_quality_check.sql
+++ scripts/00_data_quality_check.sql
@@ -7,3 +7,17 @@ WHERE
     OR issue_date IS NULL
     OR loan_amount IS NULL
     OR loan_status IS NULL
+
+-- Empty/Whitespace Check
+SELECT *
+FROM dbo.financial_loan
+WHERE 
+    LTRIM(RTRIM(address_state)) = ''
+    OR LTRIM(RTRIM(purpose)) = ''
+	OR LTRIM(RTRIM(emp_title)) = ''
+    OR LTRIM(RTRIM(grade)) = ''
+	OR LTRIM(RTRIM(sub_grade)) = ''
+	OR LTRIM(RTRIM(home_ownership)) = ''
+	OR LTRIM(RTRIM(loan_status)) = ''
+
+-- Numeric sanity Check
